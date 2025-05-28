e523e033e05f585ff6bc0bc83b13e6003bdd9c1d
montrell43 <tmerriweather@persevernow.org>
2025-05-28 15:56:06 -0500

    newly commit

--- index.js
+++ index.js
@@ -1,109 +1,184 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-// const User = require('./models/User');
-// const mongoose = require('mongoose')
 require('dotenv').config()
-// console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
 
-let uri =
-  `mongodb+srv://tmerriweather:f3NvoG0JtWPW1gY7@cluster0.eugw6oa.mongodb.net/
-  `;
-// const uri = process.env.MONGO_URI;
-// console.log('Mongo URI:', uri); // For debugging
+// const express = require('express')
+const mongoose = require('mongoose');
+// const cors = require('cors')
+// const app = express()
 
-let bodyParser = require('body-parser')
+const uri = process.env.MONGO_URI;
+console.log('Mongo URI:', uri); 
 
-let mongoose;
-try{
-  mongoose = require('mongoose');
-} catch (e) {
-  console.log(e);
-}
 
-// Mongoose set up
-mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifiedTopology: true });
-const Schema = mongoose.Schema;
 
-// Schema
-const userSchema = new Schema({
-  username: {type: String, require: true}
-})
-let userModel = mongoose.model("user", userSchema);
+//const uri = 'MONGO_URI=mongodb+srv://tmerriweather%40perseverenow.org:LETSGETIT123@cluster0.jnvwutc.mongodb.net/exercise-tracker?retryWrites=true&w=majority';
 
-app.use(cors())
-app.use(express.static('public'))
-app.use("/", bodyParser.urlencoded({ extended: false }));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+mongoose.connect(uri)
+  .then(() => {
+    console.log("mongoose connected");
+  }).catch((e) => {
+    console.log(e);
+  })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
 });
 
-// POST - create a new user
-app.post('/api/users', (req, res) => {
-  // const { username } = req.body;
-  // const user = new User({ username });
-  // await user.save();
-  // res.json({ username: user.username, _id: user._id });
-let username = req.body.username;
-let newUser = userModel({username: username});
-newUser.save();
-res.json(newUser);
+
+
+
+let Person = mongoose.model('Person', personSchema)
+
+const createAndSavePerson = (done) => {
+  const newPerson = new Person({
+    name: 'John Doe',
+    age: 30,
+    favoriteFoods: ['Pizza', 'Burger', 'Pasta']
+  });
+
+  newPerson.save((err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
+  
+};
+
+const arrayOfPeople = [
+  {name:"randall", age:37, favoriteFoods:['pizza','burger']},
+  {name:"randall", age:37, favoriteFoods:['pizza','burger']},
+  {name:"randall", age:37, favoriteFoods:['pizza','burger']}
+]
+
+const createManyPeople = (arrayOfPeople, done) => {
+
+  Person.create(arrayOfPeople, (err,people)=>{
+    if(err){
+      return done(err);
+    }
+    done(null,people);
+  })
+};
+
+const findPeopleByName = (personName, done) => {
+  Person.find({name:personName},function(err,people){
+    if(err){
+      return done(err);
+    }
+    done(null, people);
+  })
+};
+
+const findOneByFood = (food, done) => {
+  Person.findOne({favoriteFoods: food}, (err,food) =>{
+    if(err){
+      return done(err);
+    }
+    done(null, food);
+  })
+  
+};
+
+const findPersonById = (personId, done) => {
+  Person.findById(personId, (err, data)=>{
+    if(err){
+      return done(err)
+    }
+    done(null, data);
+  })
+  
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+ Person.findById(personId,(err,person)=>{
+  if(err){
+    return done(err);
+  }
+  person.favoriteFoods.push(foodToAdd);
+  person.save((err,updatedPerson)=>{
+    if(err){
+      return done(err);
+    }
+    done(null,updatedPerson);
+  })
+ })
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+  Person.findOneAndUpdate(
+    { name: personName },  // Search criteria
+    { age: ageToSet },           // Update operation
+    { new: true },         // Return the updated document
+    (err, updatedPerson) => {
+      if (err){
+        return done(err);
+      }
+      done(null, updatedPerson);
+    }
+  );
+  
+};
+
+const removeById = (personId, done) => {
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) {
+        return done(err);
+    }
+    return done(null, removedPerson);
 });
+};
 
-// GET - all users
-app.get('/api/users', async (req, res) => {
-  const users = await User.find({}, 'username _id');
-  res.json(users);
-});
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+  Person.remove({ name: nameToRemove }, (err, result) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, result);
+  });
+};
 
-// POST - add exercise
-// app.post('/api/users/:_id/exercises', async (req, res) => {
-//   const { description, duration, date } = req.body;
-//   const user = await User.findById(req.params._id);
-//   const exerciseDate = date ? new Date(date) : new Date();
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+  
+  // Build the query with chaining syntax
+  Person.find({ favoriteFoods: foodToSearch })  // Find people who like the specified food
+    .sort({ name: 1 })                          // Sort by name in ascending order
+    .limit(2)                                   // Limit the results to 2 documents
+    .select('-age')                             // Exclude the 'age' field
+    .exec((err, docs) => {                      // Execute the query and pass the callback
+      if (err) {
+        console.error('Error occurred:', err); // Log the error
+        return done(err);
+      }
+      console.log('Found documents:', docs);    // Log the results
+      done(null, docs);                         // Pass the results to the done callback
+    });
+};
 
-//   const exercise = {
-//     description,
-//     duration: parseInt(duration),
-//     date: exerciseDate.toDateString()
-//   };
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
 
-//   user.log.push(exercise);
-//   await user.save();
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
-//   res.json({
-//     username: user.username,
-//     description: exercise.description,
-//     duration: exercise.duration,
-//     date: exercise.date,
-//     _id: user._id
-//   });
-// });
-
-// GET - user logs
-// app.get('/api/users/:_id/logs', async (req, res) => {
-//   const { from, to, limit } = req.query;
-//   const user = await User.findById(req.params._id);
-
-//   let logs = [...user.log];
-
-//   if (from) logs = logs.filter(e => new Date(e.date) >= new Date(from));
-//   if (to) logs = logs.filter(e => new Date(e.date) <= new Date(to));
-//   if (limit) logs = logs.slice(0, +limit);
-
-//   res.json({
-//     username: user.username,
-//     count: logs.length,
-//     _id: user._id,
-//     log: logs
-//   });
-// });
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
