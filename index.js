e02eeeea1793993b78c2fc8c9e09ed142112def6
montrell43 <tmerriweather@persevernow.org>
2025-05-28 11:54:20 -0500

    new commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const User = require('./models/User');
-const mongoose = require('mongoose')
+// const User = require('./models/User');
+// const mongoose = require('mongoose')
 require('dotenv').config()
-let bodyParser = require('body-parser')
 // console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
 
+let uri =
+  `mongodb+srv://tmerriweather:f3NvoG0JtWPW1gY7@cluster0.eugw6oa.mongodb.net/
+  `;
+// const uri = process.env.MONGO_URI;
+// console.log('Mongo URI:', uri); // For debugging
 
-const uri = process.env.MONGO_URI;
-console.log('Mongo URI:', uri); // For debugging
+let bodyParser = require('body-parser')
 
 let mongoose;
 try{
@@ -19,22 +22,20 @@ try{
 }
 
 // Mongoose set up
-mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnfiledTopology: true });
+mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifiedTopology: true });
 const Schema = mongoose.Schema;
 
 // Schema
-const userSchema = new Schemaa({
+const userSchema = new Schema({
   username: {type: String, require: true}
 })
 let userModel = mongoose.model("user", userSchema);
 
-
-
 app.use(cors())
 app.use(express.static('public'))
-app.use("/", bodyParser.urlencoded({ extended: file }));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use("/", bodyParser.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -46,7 +47,7 @@ app.post('/api/users', (req, res) => {
   // const user = new User({ username });
   // await user.save();
   // res.json({ username: user.username, _id: user._id });
-let username = userModel({username: username});
+let username = req.body.username;
 let newUser = userModel({username: username});
 newUser.save();
 res.json(newUser);
@@ -59,47 +60,47 @@ app.get('/api/users', async (req, res) => {
 });
 
 // POST - add exercise
-app.post('/api/users/:_id/exercises', async (req, res) => {
-  const { description, duration, date } = req.body;
-  const user = await User.findById(req.params._id);
-  const exerciseDate = date ? new Date(date) : new Date();
+// app.post('/api/users/:_id/exercises', async (req, res) => {
+//   const { description, duration, date } = req.body;
+//   const user = await User.findById(req.params._id);
+//   const exerciseDate = date ? new Date(date) : new Date();
 
-  const exercise = {
-    description,
-    duration: parseInt(duration),
-    date: exerciseDate.toDateString()
-  };
+//   const exercise = {
+//     description,
+//     duration: parseInt(duration),
+//     date: exerciseDate.toDateString()
+//   };
 
-  user.log.push(exercise);
-  await user.save();
+//   user.log.push(exercise);
+//   await user.save();
 
-  res.json({
-    username: user.username,
-    description: exercise.description,
-    duration: exercise.duration,
-    date: exercise.date,
-    _id: user._id
-  });
-});
+//   res.json({
+//     username: user.username,
+//     description: exercise.description,
+//     duration: exercise.duration,
+//     date: exercise.date,
+//     _id: user._id
+//   });
+// });
 
 // GET - user logs
-app.get('/api/users/:_id/logs', async (req, res) => {
-  const { from, to, limit } = req.query;
-  const user = await User.findById(req.params._id);
+// app.get('/api/users/:_id/logs', async (req, res) => {
+//   const { from, to, limit } = req.query;
+//   const user = await User.findById(req.params._id);
 
-  let logs = [...user.log];
+//   let logs = [...user.log];
 
-  if (from) logs = logs.filter(e => new Date(e.date) >= new Date(from));
-  if (to) logs = logs.filter(e => new Date(e.date) <= new Date(to));
-  if (limit) logs = logs.slice(0, +limit);
+//   if (from) logs = logs.filter(e => new Date(e.date) >= new Date(from));
+//   if (to) logs = logs.filter(e => new Date(e.date) <= new Date(to));
+//   if (limit) logs = logs.slice(0, +limit);
 
-  res.json({
-    username: user.username,
-    count: logs.length,
-    _id: user._id,
-    log: logs
-  });
-});
+//   res.json({
+//     username: user.username,
+//     count: logs.length,
+//     _id: user._id,
+//     log: logs
+//   });
+// });
 
 
 
